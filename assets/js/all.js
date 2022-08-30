2057065dd7aa41b09bf528e9ba181284a950f361
Yuna1002 <e2431666@gmail.com>
2022-08-30 14:05:14 +0800

    Update 2022-08-30T06:05:11.773Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,9 +1,11 @@
+"use strict";
+
 $(function () {
   // console.log('Hello Bootstrap5');
-  let codeSwipe = new Swiper(".codeSwiper", {
+  var codeSwipe = new Swiper(".codeSwiper", {
     autoplay: {
       disableOnInteraction: false,
-      delay: 0,
+      delay: 0
     },
     speed: 2000,
     loop: true,
@@ -11,31 +13,29 @@ $(function () {
     slidesPerView: 3,
     breakpoints: {
       768: {
-        slidesPerView: 6,
+        slidesPerView: 6
       },
       1200: {
-        slidesPerView: 9,
-      },
-    },
-  });
+        slidesPerView: 9
+      }
+    }
+  }); // feedback-swiper
 
-  // feedback-swiper
-  const swiper = new Swiper(".feedback-swiper", {
+  var swiper = new Swiper(".feedback-swiper", {
     // Optional parameters
     effect: "fade",
     fadeEffect: {
-      crossFade: true,
+      crossFade: true
     },
-
     // If we need pagination
     pagination: {
-      el: ".swiper-pagination",
+      el: ".swiper-pagination"
     },
-
     // Navigation arrows
     navigation: {
       nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
+      prevEl: ".swiper-button-prev"
+    }
   });
 });
+//# sourceMappingURL=all.js.map
