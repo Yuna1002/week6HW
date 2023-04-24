6742b8f41742b679daca0d00b3a2a2b036c7e117
Yuna1002 <e2431666@gmail.com>
2023-04-24 21:40:29 +0800

    Update 2023-04-24T13:40:27.454Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -4,7 +4,7 @@ $(function () {
   // console.log('Hello Bootstrap5');
   var codeSwipe = new Swiper(".codeSwiper", {
     autoplay: {
-      disableOnInteraction: false,
+      // disableOnInteraction: false,
       delay: 0
     },
     speed: 2000,
@@ -21,7 +21,7 @@ $(function () {
     }
   }); // feedback-swiper
 
-  var swiper = new Swiper(".feedback-swiper", {
+  var feedbackSwiper = new Swiper(".feedbackSwiper", {
     // Optional parameters
     effect: "fade",
     fadeEffect: {
@@ -36,6 +36,14 @@ $(function () {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev"
     }
+  }); //Datepicker
+
+  var elem = document.querySelector('input[name="datepicker"]');
+  var datepicker = new Datepicker(elem, {
+    language: "zh-TW",
+    buttonClass: "btn",
+    //匹配bootstrap
+    autohide: true
   });
 });
 //# sourceMappingURL=all.js.map
